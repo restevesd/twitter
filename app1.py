268320da24c0df6e9fac174e07c6085b06bed064
Roberto Esteves <info@robertoesteves.com>
2021-12-14 13:48:37 -0500

    Add files via upload

--- app1.py
+++ app1.py
@@ -3,11 +3,6 @@ import tweepy
 from textblob import TextBlob
 from wordcloud import WordCloud
 import pandas as pd
-import numpy as np
-import re
-import matplotlib.pyplot as plt
-from PIL import Image
-import seaborn as sns
 import preprocessor as p
 import nltk
 from nltk.tokenize import word_tokenize
